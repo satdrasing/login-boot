f72d76b9aa89a69e2d92ff9b8657233713a95b65
satdrasing <satendra.kohli@gmail.com>
2018-01-13 23:02:07 +0530

    Initial commit

---
+++ src/main/java/com/satendra/login/loginboot/LoginBootApplication.java
@@ -0,0 +1,12 @@
+package com.satendra.login.loginboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginBootApplication.class, args);
+	}
+}
